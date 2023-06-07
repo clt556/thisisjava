338b5ad014ad0abbd44063b80c75221c40c5f112
kjw <abc@naver.com>
2023-06-07 14:32:11 +0900

    first commit on eclipse

---
+++ thisisjava/src/module-info.java
@@ -0,0 +1,2 @@
+module thisisjava {
+}
---
+++ thisisjava/src/thisisjava/helloworld.java
@@ -0,0 +1,10 @@
+package thisisjava;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ㅇㄹㄴ알ㄴㅇ린ㅇㄹ");
+	}
+
+}

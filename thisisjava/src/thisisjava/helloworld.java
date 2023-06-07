1f494c802abb666e29fd5b888a88acba0ee10bab
clt556 <116059720+clt556@users.noreply.github.com>
2023-06-07 14:40:00 +0900

    Update helloworld.java
    ㄹㄹㄹㄹㄹㄹ

--- thisisjava/src/thisisjava/helloworld.java
+++ thisisjava/src/thisisjava/helloworld.java
@@ -5,6 +5,6 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ㅇㄹㄴ알ㄴㅇ린ㅇㄹ");
-	}
+		System.out.println("깃허브에서 수정"	};
 
 }
